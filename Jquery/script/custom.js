2621f6b2ca93d7263dc73f6fab178f1e333c7595
wonjinsong <msmk22239@gmail.com>
2021-04-27 23:02:02 -0400

    jquery basic

---
+++ Jquery/script/custom.js
@@ -0,0 +1,7 @@
+$("p").css({ display: "none" });
+$(".show-btn").click(function () {
+  $("p").css({ display: "block" });
+});
+$(".hide-btn").click(function () {
+  $("p").css({ display: "none" });
+});
