f692d0298f8dcd48860b12e74ed784682af81d74
wonjinsong <msmk22239@gmail.com>
2021-05-04 22:42:58 -0400

    slideDown Up this stop()

--- Jquery/script/menu.js
+++ Jquery/script/menu.js
@@ -1,3 +1,9 @@
 $(".menu li").mouseenter(function () {
-  $(".sub-menu").slideDown();
+  // this는 .menu li중 내가 건드린 애를 가르키고, 다시 탐색해가야함
+  // stop()은 한번 실행했던 애니메이션 기억없앤다
+  $(this).children(".sub-menu").stop().slideDown();
+});
+
+$(".menu li").mouseleave(function () {
+  $(this).children(".sub-menu").stop().slideUp();
 });
